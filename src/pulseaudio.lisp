aaa8e1a95d29ed5f6e16f4ab92c407713193549b
t-sin <shinichi.tanaka45@gmail.com>
2021-02-10 01:06:03 +0900

    WIP: a function to open PulseAudio simple stream

--- src/pulseaudio.lisp
+++ src/pulseaudio.lisp
@@ -1,20 +1,47 @@
 (defpackage #:pulseaudio
   (:use #:cl
-        #:pulseaudio.cffi)
-  (:export #:with-audio-stream))
+        #:pulseaudio.cffi
+        #:pulseaudio.keyword)
+  (:export #:*available-sample-formats*
+           #:raw-sample-format
+           #:open-simple-stream
+           #:with-audio-stream))
 (in-package #:pulseaudio)
 
-(defmacro with-audio-stream ((stream appname (&key (direction :output)
-                                                   (device nil)
-                                                   (description "cl-pulseudio stream")
-                                                   (sample-spec nil)
-                                                   (channel-map nil)
-                                                   (buffer-attributes nil)
-                                                   (ignore-errors nil)))
+(defun open-simple-stream (appname direction
+                           &key device description
+                                sample-format rate channels
+                                channel-map buffer-attributes ignore-errors)
+  (let ((sample-format (raw-sample-format sample-format))
+        ;; TODO: convert to cffi keywords
+        (sample-spec nil)
+        (device nil)
+        (channel-map nil)
+        (buffer-attributes nil)
+        (ignore-errors nil))
+    (pa-simple-new nil appname direction device description
+                   sample-spec channel-map buffer-attributes ignore-errors)))
+
+(defmacro with-audio-stream ((stream &key (appname "cl-pulseaudio app")
+                                          (direction :output)
+                                          (device nil)
+                                          (description "cl-pulseudio stream")
+                                          (sample-format nil)
+                                          (rate 44100)
+                                          (channels 2)
+                                          (channel-map nil)
+                                          (buffer-attributes nil)
+                                          (ignore-errors nil))
                              &body body)
-  `(let ((,stream (pa-simple-new nil appname
-                                 direction device description
-                                 sample-format channel-map buffer-attributes ignore-errors)))
+  `(let ((,stream (open-simple-stream ,appname ,direction
+                                      :device ,device
+                                      :description ,description
+                                      :sample-format ,sample-format
+                                      :rate ,rate
+                                      :channels ,channels
+                                      :channel-map ,channel-map
+                                      :buffer-attributes ,buffer-attributes
+                                      :ignore-errors ,ignore-errors)))
      (unwind-protect
           (progn ,@body)
        (unless (cffi:null-pointer-p ,stream)
